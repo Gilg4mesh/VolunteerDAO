f0df8eb65c2bdc49cca42fd9d4c9f649915f6b8d
N.M <freedomtomdestiny@gmail.com>
2017-08-14 00:53:19 +0800

    Add files via upload
    ver.1

---
+++ js/app.js
@@ -0,0 +1,105 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new web3.providers.HttpProvider('https://kovan.infura.io');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('https://raw.githubusercontent.com/Gilg4mesh/VolunteerDAO/master/CareToken.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var CareTokenArtifact = data;
+      App.contracts.CareToken = TruffleContract(CareTokenArtifact);
+
+      // Set the provider for our contract.
+      App.contracts.CareToken.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      return App.getBalances();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#transferButton', App.handleTransfer);
+  },
+
+  handleTransfer: function() {
+    event.preventDefault();
+
+    var amount = parseInt($('#CTTransferAmount').val());
+    var toAddress = $('#CTTransferAddress').val();
+
+    console.log('Transfer ' + amount + ' CT to ' + toAddress);
+
+    var careTokenInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.CareToken.deployed().then(function(instance) {
+        careTokenInstance = instance;
+
+        return careTokenInstance.transfer(toAddress, amount, {from: account});
+      }).then(function(result) {
+        alert('Transfer Successful!');
+        return App.getBalances();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  getBalances: function(adopters, account) {
+    console.log('Getting balances...');
+
+    var careTokenInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.CareToken.deployed().then(function(instance) {
+        careTokenInstance = instance;
+
+        return careTokenInstance.balanceOf(account);
+      }).then(function(result) {
+        balance = result.c[0];
+
+        $('#CTBalance').text(balance);
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
